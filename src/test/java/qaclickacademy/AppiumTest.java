5633079a2d37b68b360198ed35d95005a82708c7
shanky2609 <shashank.krishna57@gmail.com>
2021-08-31 20:19:46 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void Androidapp()
+	{
+		System.out.println("Androidapp");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApp");
+	}
+
+}
